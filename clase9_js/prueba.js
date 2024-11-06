46f1b6649d86ff1efe3b8fe8cf020c2430928de1
alejandro <alejandrozapata73@gmail.com>
2024-11-05 21:11:04 -0300

    clase 9 js

---
+++ clase9_js/prueba.js
@@ -0,0 +1,33 @@
+
+//comentarios...
+
+/*
+esto es un comentario multilinea
+hola hola 
+hola
+
+*/
+
+//datos.. del tipo cadena de caracteres...(textos-literales)
+console.log("probando desde archivo externo")
+
+console.log("probando sumar cadena de caracteres")
+console.log("10"+"10") // concatenar cadena de caracteres
+
+//operaciones aritmeticas
+console.log(10+10+10)
+console.log(10-10)
+console.log(10*10)
+console.log(10/10)
+
+console.log("La suma de 2 + 2 es: "  +  (2+2) )
+//operacion X
+
+console.log((2+2) + (4-5))
+console.log(5+5-6+8 * (4-5) / (5/5))
+
+//tipo de datos... Booleanos
+
+console.log(true) // prendido - 1
+console.log(false) // apagado - 0 operaciones logicas
+
