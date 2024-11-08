63103319e7c6088c2601f6632c99570244493579
alejandro <alejandrozapata73@gmail.com>
2024-11-07 22:01:16 -0300

    after js

--- clase9_js/prueba.js
+++ clase9_js/prueba.js
@@ -29,5 +29,81 @@ console.log(5+5-6+8 * (4-5) / (5/5))
 //tipo de datos... Booleanos
 
 console.log(true) // prendido - 1
-console.log(false) // apagado - 0 operaciones logicas
+console.log(false) // apagado - 0 operaciones  logicas
+ 
 
+//usuario y contraseña = verdad
+//homebanking == false .... true
+
+//programar.....Algoritmo----Programa
+
+/*
+saldo = 2000
+1k pan = resta
+*/
+
+
+
+
+console.log("Cristian Acosta tienen 32")
+console.log("cristian")
+console.log("Christian")
+
+//variable.. un espacio en memoria (caja)
+
+var nombreUsuario = "Cristian"
+var correo_usuario = "cristian@gmail.com"
+var edad = 32
+var soyRobot = false
+
+console.log("el nombre del usuairo es: " + nombreUsuario)
+
+
+//generar un algoritmo que sume dos varia
+
+console.log("***Suma de numeros: ")
+
+const IVA = 21
+var numero1 = 50
+var numero2 = 50
+
+var sumaNumeros = numero1 + numero2 // suma numeros lo voy a necesitar a futuro
+
+console.log("la suma de los dos numeros es: " + sumaNumeros)//resultado.. suma
+
+numero1 = 80
+numero2 = 100
+
+IVA = 30
+
+console.log(numero1)
+console.log(numero1)
+numero1 = 90
+console.log(numero1)
+console.log(numero2)
+
+var saldo = 20000
+
+var transferencia = 5000
+
+saldo = 0
+
+//.................0   +  5000
+var saldoFinal = saldo + transferencia
+
+console.log("mi saldo actual es: " + saldoFinal)
+
+/*
+
+IVA
+DNI
+GRAVEDAD
+PI
+
+*/
+
+//video juegos...
+
+var vidas = 3
+
+var tortuga = 
---
+++ clase9_js_after/script1.js
@@ -0,0 +1,62 @@
+console.log("hola soy el script 1")
+console.log(10)
+console.log(true)
+console.log(false)
+
+const PERSONA1 = "Mario"
+const PERSONA2 = "luigui" 
+const TORTUGAS = 1
+
+var nombre = PERSONA1
+var vidas = 3
+
+
+console.log(PERSONA1)//que imprime
+console.log(nombre)//que imprime
+
+console.log("****Bienvenidos a Super Wario*****")
+
+console.log("su persojane elegido es: " + nombre)
+console.log("la vida actual de: " + nombre + " es: " + vidas)
+
+vidas = vidas-TORTUGAS
+
+console.log(vidas)
+
+vidas = vidas-TORTUGAS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
