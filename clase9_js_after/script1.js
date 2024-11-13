da1bd6b8a650f6a60e85ef983a9c637bb8493b0c
alejandro <alejandrozapata73@gmail.com>
2024-11-12 21:08:50 -0300

    clase10 primera parte

--- clase9_js_after/script1.js
+++ clase9_js_after/script1.js
@@ -1,46 +1,91 @@
-console.log("hola soy el script 1")
-console.log(10)
-console.log(true)
-console.log(false)
+// console.log("hola soy el script 1")
+// console.log(10)
+// console.log(true)
+// console.log(false)
 
-const PERSONA1 = "Mario"
-const PERSONA2 = "luigui" 
-const TORTUGAS = 1
+// const PERSONA1 = "Mario"
+// const PERSONA2 = "luigui" 
+// const TORTUGAS = 1
 
-var nombre = PERSONA1
-var vidas = 3
-
-
-console.log(PERSONA1)//que imprime
-console.log(nombre)//que imprime
-
-console.log("****Bienvenidos a Super Wario*****")
-
-console.log("su persojane elegido es: " + nombre)
-console.log("la vida actual de: " + nombre + " es: " + vidas)
-
-vidas = vidas-TORTUGAS
-
-console.log(vidas)
-
-vidas = vidas-TORTUGAS
+// var nombre = PERSONA1
+// var vidas = 3
 
 
-console.log("app de mercado libre")
+// console.log(PERSONA1)//que imprime
+// console.log(nombre)//que imprime
+
+// console.log("****Bienvenidos a Super Wario*****")
+
+// console.log("su persojane elegido es: " + nombre)
+// console.log("la vida actual de: " + nombre + " es: " + vidas)
+
+// vidas = vidas-TORTUGAS
+
+// console.log(vidas)
+
+// vidas = vidas-TORTUGAS
 
 
-var saldo = 30000
-
-const ZAPATA = 15000
-const fuente = 15000
+// console.log("app de mercado libre")
 
 
-var stockZapata = 300
-stockZapata = 300 - TORTUGAS
+// var saldo = 30000
 
-var descuento = 15
+// const ZAPATA = 15000
+// const fuente = 15000
 
-saldo = saldo - ZAPATA - fuente 
+
+// var stockZapata = 300
+// stockZapata = 300 - TORTUGAS
+
+// var descuento = 15
+
+// saldo = saldo - ZAPATA - fuente 
+
+
+// var vidas = 3
+
+// console.log(vidas)
+
+// const DESCONTAR = 1
+
+// vidas = vidas - DESCONTAR
+
+// var vidas = 8
+// var vidas = 10
+
+// console.log(vidas)
+
+// let numero1 = 1
+// let numero2 = 2
+// console.log("imprimiendo con variable let")
+
+// let nombreUser = prompt("ingrese un mumero")
+// let apellidoUser = prompt ("ingrese otro numero")
+
+// nombreUser = Number(nombreUser)
+// console.log(typeof nombreUser)
+
+// apellidoUser = Number(apellidoUser)
+// console.log(typeof apellidoUser)
+
+// let sumaNumeros = nombreUser + apellidoUser
+
+// console.log(typeof sumaNumeros)
+
+// console.log(sumaNumeros)
+
+
+let edad1 = parseInt(prompt("ingrese su edad"))
+
+let peso = parseFloat(prompt("ingrese otra edad"))
+
+//...meses... anio nacimiento... dni...personas..claves..pin..hijos...numerosClientes
+
+document.write("la edad del usuario es: " + edad1)
+console.log(edad1)
+alert("la edad el usuario es:" + edad1)
+
 
 
 
