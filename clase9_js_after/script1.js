c5a3d8b103581eba15fc0c52916b5651f93cfa59
alejandro <alejandrozapata73@gmail.com>
2024-11-12 22:05:29 -0300

    segunda parte condicionales

--- clase9_js_after/script1.js
+++ clase9_js_after/script1.js
@@ -76,18 +76,82 @@
 // console.log(sumaNumeros)
 
 
-let edad1 = parseInt(prompt("ingrese su edad"))
+// let edad1 = parseInt(prompt("ingrese su edad"))
 
-let peso = parseFloat(prompt("ingrese otra edad"))
+// let peso = parseFloat(prompt("ingrese otra edad"))
 
-//...meses... anio nacimiento... dni...personas..claves..pin..hijos...numerosClientes
+// //...meses... anio nacimiento... dni...personas..claves..pin..hijos...numerosClientes
 
-document.write("la edad del usuario es: " + edad1)
-console.log(edad1)
-alert("la edad el usuario es:" + edad1)
+// document.write("la edad del usuario es: " + edad1)
+// console.log(edad1)
+// alert("la edad el usuario es:" + edad1)
+
+//operadores de comparacion..
+
+/*
+igual ==
+mayor >
+mayor o igual >=
+menor <
+menor o igual <=
+distinto !=
+
+*/
+
+console.log(1==2)//booleando
+console.log(10>9)//true
+console.log(8<15)//true
+console.log(8!=8)//
+console.log("casa"=="casa")
+console.log("casa"=="Casa")
+console.log(1=="1")
+console.log(1==="1")
+console.log("es verdad... si la verad si..")
+console.log("C"<"c")
+
+console.log("pedido de informacion")
+
+let usuarioTabla = "ale"
+let passTabla = "123456"
+
+let usuario = prompt("ingrese quien va a ser el ganador:")
+// let pass = prompt("ingrese su password")
 
 
 
+console.log(usuario==usuarioTabla)//
+// console.log(pass==passTabla)
+
+// si hago la pre entrega...apruebo.. si no entrego..te llama ale dicienteo 7 dias
+
+
+//resultado booleano
+
+let valor = true
+
+let vida = 0
+
+let cuotaNoviembre = true
+let diasApagar = 31
+
+if(usuario==usuarioTabla) {
+    console.log("pasa algo")
+    alert("hola es verda")
+    document.write("holaaa pasaste la prueba")
+    if(cuotaNoviembre==false) {
+        alert("me debes plata man!!!")
+    }else {
+        alert("estas al dia...")
+    }
+   
+
+    
+}else {
+
+    alert("usuario incorrecto")
+    document.write("<h1>usuario incorrecto</h1>")
+
+}
 
 
 
