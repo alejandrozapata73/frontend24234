6cb0b94a58ea8afdc3bf5c605f071ece2bd65027
alejandro <alejandrozapata73@gmail.com>
2024-11-19 22:03:22 -0300

    ciclos

--- clase11/script.js
+++ clase11/script.js
@@ -16,99 +16,219 @@
 //     document.write("es correcto")
 // }else{
 //     document.write("es el pass incorrecto")
+// // }
+
+
+// //operadores logicos
+// /* And --- y
+
+// true and true   = true
+// true and false  = false
+// false and true  = false
+// false and false = false
+
+// 1 && 1 = 1
+// 0 && 1 = 0
+// 1 && 0 = 0
+// 0 && 0 = 0
+
+
+// */
+
+// console.log(4<3)//false
+// console.log(10<10)//false
+// //...........false  false = 
+// console.log((4<3)&&(10<9))
+// //         true       true = true
+// console.log((5>3) && (6>4))
+
+// //         false     true = false
+//  console.log((4>6)&&(5>3))
+
+
+//  const usuarioBD = "ale"
+// //  const passBD = "123456"
+
+
+// //  console.log(usuarioBD == usuario)//false
+
+// //  let usuario = prompt("ingrese su usuario")
+// //  let pass = prompt("ingrese su pass")
+
+// // //............ale      == ale ... true
+// //  console.log(usuarioBD == usuario)//false true
+ 
+
+// // //.......1
+
+// // /*
+
+// // usuarioBD  && usuario =
+// // "ale"      &&   "ale"  = true
+
+// // */
+
+
+
+// //  if((usuario == usuarioBD) && (pass==passBD)){
+// //     alert("ingresaste al sistema!!!!")
+// //  }else{
+// //     alert("no tiene el ingresooooo")
+// //  }
+
+// //  //prueba de escritorio....
+
+// // let checkRobot = true
+
+
+// //  if((correo == netflixBDusuario) && (pass == netflixBDpass)){
+// //     console.log("puede ver algo")
+// //  }
+
+// //  //operadores logicos
+// // /* OR --- 
+
+// // // true or true   = true
+// // // true or false  = true
+// // // false or true  = true
+// // // false or false = false
+
+// // // 1 && 1 = 1
+// // // 0 && 1 = 0
+// // // 1 && 0 = 0
+// // // 0 && 0 = 0
+
+
+// // // */
+
+// // // //...........................1........................and.........1
+// // // //...()...1................or...........1...........) and (........1....)
+// // // if( ((correo == correoBD) or (telefono == telefonBD)) && (pass == passBD)){
+
+// // // }
+
+
+// // // console.log((1>2)) //false
+// // // console.log(1>3) //false
+// // // console.log(4>3)//true
+
+ 
+// // //           1       0     == 1
+// // console.log((4>3)||(4>10))
+// // //           0       1     == 1
+// // console.log((4>5)||(15>10))
+// // //
+// // console.log((4>3)||(4>10))
+// // //
+// // console.log((4>3)||(4>10))
+
+
+// // let dia = parseInt(prompt("ingrese un dia de la semana"))
+
+
+// // if(dia==1){
+// //     alert("es lunes")
+// // } else if(dia==2){
+// //     alert("es martes")
+// // } else if(dia == 3){
+// //     alert("es miercoles")
+// // }else {
+// //     alert("noi es ningun dia")
+// // }
+
+
+// // switch(dia){
+
+// //     case 1:
+// //         alert("es lunes")
+// //     break
+// //     case 2:
+// //         alert("es martes")
+// //     break
+
+// //     case 3:
+// //         alert("es miercoles")
+// //     break
+
+// //     default:
+// //         alert("no ingresso un numero entre 1 y 7")
+// //     break
+
+// // }
+
+// // //cajero...
+
+// let saldo = 1
+
+// let alias 
+
+// let opcion = parseInt(prompt("ingfrese una opcion entre 1 y 4"))
+
+
+// switch(opcion){
+
+//     case 1:
+//         alert("verificar saldo" + saldo)
+
+//         if(saldo<=0){
+//             alert("no llego a fin de mes")
+//         }
+//     break
+//     case 2:
+//         alert("puede extraerr dinero")
+//         /*
+
+//         */
+//     break
+//     case 3:
+//         alert("transferencia")
+//     break
+
+//     default:
+//         alert("numero incorrecto")
+//     break
+
 // }
 
 
-//operadores logicos
-/* And --- y
+//ciclos......
 
-true and true   = true
-true and false  = false
-false and true  = false
-false and false = false
+let argentina = 0
 
-1 && 1 = 1
-0 && 1 = 0
-1 && 0 = 0
-0 && 0 = 0
+//       1     >0 = true
+
+argentina = 10
+
+while(argentina<5){
 
 
-*/
+let numero = parseInt(prompt("ingrese un numero"))
 
-console.log(4<3)//false
-console.log(10<10)//false
-//...........false  false = 
-console.log((4<3)&&(10<9))
-//         true       true = true
-console.log((5>3) && (6>4))
-
-//         false     true = false
- console.log((4>6)&&(5>3))
-
-
- const usuarioBD = "ale"
- const passBD = "123456"
-
-
- console.log(usuarioBD == usuario)//false
-
- let usuario = prompt("ingrese su usuario")
- let pass = prompt("ingrese su pass")
-
-//............ale      == ale ... true
- console.log(usuarioBD == usuario)//false true
- 
-
-//.......1
-
-/*
-
-usuarioBD  && usuario =
-"ale"      &&   "ale"  = true
-
-*/
-
-
-
- if((usuario == usuarioBD) && (pass==passBD)){
-    alert("ingresaste al sistema!!!!")
- }else{
-    alert("no tiene el ingresooooo")
- }
-
- //prueba de escritorio....
-
-let checkRobot = true
-
-
- if((correo == netflixBDusuario) && (pass == netflixBDpass)){
-    console.log("puede ver algo")
- }
-
- //operadores logicos
-/* OR --- 
-
-true or true   = true
-true or false  = true
-false or true  = true
-false or false = false
-
-1 && 1 = 1
-0 && 1 = 0
-1 && 0 = 0
-0 && 0 = 0
-
-
-*/
-
-//...........................1........................and.........1
-//...()...1................or...........1...........) and (........1....)
-if( ((correo == correoBD) or (telefono == telefonBD)) && (pass == passBD)){
+if(numero == 3){
+    alert("adivinaste el numero")
+    break
+}
 
 
 }
 
 
-console.log((1>2)) false
-console.log(1>3) false
-console.log(4>3)//trure
+argentina = 0
+
+
+do{
+
+   
+ alert("hola soy un while sin permiso")
+
+  
+ argentina++
+
+} while(argentina<5)
+
+
+
+
+
+
+
