f188014dd91e0ba0bbc55e05096852c350dc265c
alejandro <alejandrozapata73@gmail.com>
2024-12-03 22:15:54 -0300

    eventos js

--- clase13/script.js
+++ clase13/script.js
@@ -1,29 +1,103 @@
 
 
-// function alejoCartelito(){
+// // function alejoCartelito(){
 
-//     alert("hola probando")
-//     console.log("estoy sumando + +++")
-// }
+// //     alert("hola probando")
+// //     console.log("estoy sumando + +++")
+// // }
 
-// function spamPublicidad(){
-//     var nombre = prompt("ingrese su numero de targ credit")
+// // function spamPublicidad(){
+// //     var nombre = prompt("ingrese su numero de targ credit")
 
-//     console.log("tu nombre es: " + nombre)
+// //     console.log("tu nombre es: " + nombre)
 
-//     alert("estas escribiendo tu nombre " + nombre)
+// //     alert("estas escribiendo tu nombre " + nombre)
 
-//     alert("te gasnaste un premio pres here!!!")
-
-    
-// }
-
-var contadorA = 1000000
-
-function contador(){
+// //     alert("te gasnaste un premio pres here!!!")
 
     
-    contadorA++
-    console.log(contadorA)
+// // }
+
+// // var contadorA = 1000000
+
+// // function contador(){
+
+    
+// //     contadorA++
+// //     console.log(contadorA)
+
+// // }
+
+
+// //
+// let capturaH1 = document.getElementById("titulo")
+
+// let capturaH2 = document.getElementById("elemento2")
+
+// console.log(capturaH2)
+// console.log(capturaH2.textContent)
+
+
+
+
+
+//    ingresoEdad = parseInt(prompt("ingrese una edad"))
+
+
+
+// if(ingresoEdad>=18){
+
+//  capturaH2.textContent = "probando de a uno!!!"
+
+// }
+
+// // function retornarObjeto() {
+
+// //     console.log(capturaH1.textContent)
+// // }
+
+// // function cambiarValor(){
+
+// //     document.getElementById("titulo").textContent = "Voila!!!!"
+
+// // }
+
+// // //id = 1 elemento
+
+// // function eliminarElemento(){
+
+// //     document.getElementById("titulo").textContent = "Voila!!!!"
+
+// // }
+
+
+// // //pidiendo que retorne algo....
+// // console.log(document.getElementById("titulo"))
+// // //
+// // console.log(document.getElementById("titulo").textContent)
+
+let capturaInputA = document.getElementById("inputA")
+let capturaInputB = document.getElementById("inputB")
+let capturaCartel3 = document.getElementById("pruebaListener")
+
+
+
+
+function mostrarValor(){
+
+    console.log(capturaInputA.value)
+    console.log(capturaInputB.value)
+    var suma = capturaInputA.value + capturaInputB.value
+    console.log(suma)
+   
+    capturaCartel3.textContent = suma
 
 }
+
+
+
+console.log(capturaInputA)
+
+console.log(capturaInputA.textContent)
+
+
