2c3ce6f1ed0d1f31897d39129f49504378cd7942
alejandro <alejandrozapata73@gmail.com>
2024-12-17 22:23:26 -0300

    funciones y conexion a API

--- clase15/script.js
+++ clase15/script.js
@@ -1,199 +1,166 @@
-console.log("hola probando event listener")
+// //funciones flecha....
+
+// function saludo(mensaje){
+//     return mensaje
+// }
 
 
 
 
-localStorage.setItem("saludo","hola usuario")
+// let retornoFuncion = saludo("hola estoy probando")
 
-localStorage.setItem("usuario","alejandro")
+// console.log(retornoFuncion)
 
-let nombreUsuario = localStorage.getItem("usuario")
 
-console.log(nombreUsuario)
+// const saludoReducido = function (mensaje) {
+//     return mensaje
+// }
 
-localStorage.removeItem("usuario")
+// const saludoReducido1 = (mensaje) => {
+//     return mensaje }
 
-console.log("luego de remover item")
+// const saludoReducido2 = (mensaje,valor) => mensaje 
+       
 
-nombreUsuario = localStorage.getItem("usuario")
+// saludoReducido("algun parametro X")
 
-console.log(nombreUsuario)
 
-if(nombreUsuario==null){
-    alert("El local Storage esta vacio")
-}else{
-    alert("tiene algo que lo llena")
+
+let alumnosTT = [
+    {
+
+        id: 1,
+        nombre: "Rodrigo",
+        apellido: "farias",
+        curso: true
+    },
+    {
+
+        id: 2,
+        nombre: "Francisco",
+        apellido: "farias",
+        curso: true
+    },
+    {
+
+        id: 3,
+        nombre: "Roxana",
+        apellido: "farias",
+        curso: false
+    },
+]
+
+//.............0...
+let numeros = [10,20,30,40,50,60,80,90]
+
+console.log("recorriendo el array con un for")
+for(let i=0;i<numeros.length;i++){
+    console.log(numeros[i])
 }
 
-localStorage.clear()
-
-
-
-
-let botonB = document.getElementById("inputB")
-
-botonB.addEventListener("click", function(){
-    console.log("hola probando event listener")
-})
-
-botonB.addEventListener("keyup", function(event){
-    console.log(event)
-    var inputText = event.key ; 
-    document.getElementById("textArea").innerHTML += inputText;
-})
-
-//analiza los cambios luego de salir del input
-botonB.addEventListener("change", function(){
-    console.log("estoy recibiendo cambios")
-})
-
-//es cuando pierde el foco
-botonB.addEventListener("blur", function(){
-    console.log("estoy recibiendo cambios de blur")
-})
-
-botonB.addEventListener("focus", function(){
-    console.log("estoy recibiendo cambios de foco")
-})
-
-
-function suma(a){
-    return a
+for(let i=0;i<alumnosTT.length;i++){
+    console.log(alumnosTT[i])
 }
 
-let resultSuma = (a)=>  a
+//foreach
 
-console.log(resultSuma(10))
+console.log("implementando foreach para recorrer un array!!!")
+numeros.forEach(elemento =>console.log(elemento))
+
+//recoriendo array con foreach
+console.log("*****recorriendo array objetos con foreach***")
+alumnosTT.forEach(elemento=>console.log(elemento.nombre))
+
+
+//asincronismo....
+/*
+
+
+
+*/
+
+let contenedorPersonajes = document.getElementById("contenedorPersonajes")
+console.log(contenedorPersonajes)
+
+//funcion fech me permite contectar con una API-end Point
+fetch("https://rickandmortyapi.com/api/character")
+//tranformo la info en formato json
+.then((respuesta)=>respuesta.json())
+//en datos tengo ya la estructura para mostrar en formato objeto
+.then((datos)=>{
+    console.log(datos)
+    console.log(datos.results)
+    console.log(datos.results[0])
+    console.log(datos.results[0].name)
+
+    datos.results.forEach((elemento)=>{
+        // console.log(elemento)
+        console.log(elemento.name)
+        console.log(elemento.status)
+
+        const contenedorCreado = document.createElement("div")
+
+        contenedorCreado.innerHTML = `
+
+            <h4>${elemento.name}</h4>
+            <img src="${elemento.image}">
+
+            `
+            ;
+
+        contenedorPersonajes.append(contenedorCreado)
+
+
+    })
+
+})
+
+//traer info de archivo Json
+
+let contenedorJson = document.getElementById("contenedorJson")
+console.log(contenedorPersonajes)
+
+//funcion fech me permite contectar con una API-end Point
+fetch("empleados.json")
+//tranformo la info en formato json
+.then((respuesta)=>respuesta.json())
+//en datos tengo ya la estructura para mostrar en formato objeto
+.then((datos)=>{
+    console.log(datos)
+    console.log(datos[0])
+    console.log(datos[0].nombre)
+    // console.log(datos.results)
+    // console.log(datos.results[0])
+    // console.log(datos.results[0].name)
+
+    datos.forEach((elemento)=>{
+        // console.log(elemento)
+        console.log(elemento.nombre)
+
+        const contenedorCreado = document.createElement("div")
+
+        contenedorCreado.innerHTML = `
+
+            <h4>${elemento.nombre}</h4>
+            
+
+            `
+            ;
+
+            contenedorJson.append(contenedorCreado)
+
+
+    })
+
+})
 
 
 
 /*
 
-Mouse Events: click, dblclick, mousedown, mouseup, contextmenu, mouseout, mousewheel, mouseover
-Touch Events: touchstart, touchend, touchmove, touchcancel
-Keyboard Events: keydown, keyup, keypress
-Form Events: focus, blur, change, submit
-Window Events: resize, scroll, load, unload, hashchange
+1-mostrar datos de un array en html...
+2-mostrar datos de una api
+3-mostrar datos de un archivo json
 
 
 */
-
-
-const paises = ["Argentina","Brasil"]
-
-console.log("array antes de hacer un push: ")
-console.log(paises)
-
-paises.push("alemania")
-
-console.log("array luego de hacer un push")
-
-console.log(paises)
-
-
-
-paises.unshift("Portugal")
-
-console.log("array luego de ingresar en el centro con unshift:")
-
-console.log(paises)
-
-paises.shift()
-
-console.log("array luego de eliminar el de inicio:")
-
-console.log(paises)
-
-
-const numeros = [1,2,4,5,6]
-
-const numerosModificados = numeros.map(num => num + 1)
-
-console.log(numeros)
-console.log(numerosModificados)
-
-const icrementoImpuesto = numeros.map(num => num + 10)
-
-console.log(icrementoImpuesto)
-
-console.log(numeros.map(num => num + 20))
-
-//suma de elementos en un array
-let sumaFor = 0
-
-numeros.forEach(valorIndice => {
-
-    sumaFor = sumaFor + valorIndice
-})
-
-console.log("valor total suma: ", sumaFor)
-
-
-const contenedorPersonajes = document.getElementById("contenedorPersonajes")
-
-console.log(contenedorPersonajes)
-
-fetch("https://rickandmortyapi.com/api/character")
-.then((response)=>response.json())
-.then((datos)=>{
-    console.log(datos)
-    console.log(datos.results)
-
-    datos.results.forEach((elementos)=>{
-
-        console.log(elementos.name)
-
-        const contenedorCreado = document.createElement('div')
-
-        contenedorCreado.innerHTML = `
-        <h4> ${elementos.name}</4>
-        `;
-
-        contenedorPersonajes.append(contenedorCreado)
-    })
-})
-
-//contenedor Random
-
-let contenedorRandom = document.getElementById("contenedorPersonajes2")
-
-fetch("https://randomuser.me/api/")
-.then((response)=>response.json())
-.then(data=>{
-    console.log(data)
-    console.log(data.results[0])
-    console.log(data.results[0].gender)
-    console.log(data.results[0].name)
-    console.log(data.results[0].name.first)
-})
-
-
-//contenedor Json
-
-const contenedorJson = document.getElementById("contenedorJson")
-
-console.log(contenedorJson)
-
-fetch("empleados.json")
-.then((response)=>response.json())
-.then((datos)=>{
-    console.log("+++++++++++json")
-    console.log(datos)
-
-
-    datos.forEach((elementos)=>{
-
-        console.log(elementos.nombre)
-
-        const contenedorCreado = document.createElement('div')
-
-        contenedorCreado.innerHTML = `
-        <h4> ${elementos.nombre}</4>
-        `;
-
-        contenedorJson.append(contenedorCreado)
-    })
-})
-.catch(function (error){
-    console.log("Error Asincronia", error)
-})
